9379167805c49362f4ceecfc81bc33d127f7bc1e
Daniel Armbruster <danielgundam00@gmail.com>
2022-02-21 16:05:42 -0700

    commit tree 8272674ba7332a2f46552c4b0ba4b2d2500bd359

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if(num <= 400){
+    return "This one is on me!";
+  }else if(num > 2500){
+    return 'No can do.';
+  }else if(num > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }else{
+    return;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(string){
   // Write your code here!
+  return string === 'NYC'? 'Ok, sounds good.': 'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(string){
   // Write your code here!
+  switch(string){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
